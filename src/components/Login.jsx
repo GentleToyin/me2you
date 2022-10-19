257315ff01fdbdf89df5af6efaee9177ed58b275
GentleToyin <41758364+GentleToyin@users.noreply.github.com>
2022-10-19 23:44:50 +0100

    Google auth added

---
+++ src/components/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+// import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logo.png";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+const Login = () => {
+  // const [decoded, setDecoded] = useState()
+  const responseGoogle = (response) => {
+    let decoded = jwt_decode(response.credential);
+    console.log(decoded);
+  };
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className=" absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} width="130px" alt="logo" className="m-auto mb-3" />
+          <div className="shadow-2xl">
+            {/* <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            /> */}
+            <GoogleLogin
+              onSuccess={responseGoogle}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
